c5d844bfcd8572c7af91981e4008c62683864378
wbpxre150 <100937007+wbpxre150@users.noreply.github.com>
2025-11-06 10:14:52 +0800

    Update version to 11.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.wbpxre150.shoppinglist"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "10.0"
+        versionCode = 11
+        versionName = "11.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
